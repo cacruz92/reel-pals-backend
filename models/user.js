79862a0c06f6f4f3dc93f4c4cf10f68085f5cfca
Christian Cruz <christian.cruz92@hotmail.com>
2024-07-30 19:11:47 -0400

    I have created a user class and within it there are static methods for register, login, and get.

--- models/user.js
+++ models/user.js
@@ -1,1 +1,103 @@
-const db = require
+const db = require("../db");
+const {
+    NotFoundError, 
+    BadRequestError, 
+    UnauthorizedError
+} = require("../expressError")
+
+class User {
+    /**  authenticate user with username, password.
+    *returns {username, first_name, last_name, email, birthday}
+    * throws UnauthorizedError if user not found or wrong password
+    **/
+
+    static async authenticate(username, password){
+        const result = await db.query(
+            `SELECT username,
+                    password,
+                    email, 
+                    first_name AS "firstName", 
+                    last_name AS "lastName",
+                    birthday
+            FROM users
+            WHERE username = $1`,
+            [username]
+        );
+        
+        const user = result.rows[0];
+
+        if (user) {
+            if(user.password === password){
+                delete user.password;
+                return user;
+            }
+        }
+        throw new UnauthorizedError("Invalid username/password")
+    }
+
+    /** register user with data
+     * returns {username, first_name, last_name, email, birthday}
+     * Throws BadRequestError on duplicates
+    */
+
+    static async register({username, password, email, firstName, lastName, birthday}){
+        // check for duplicates
+        const duplicateCheck = await db.query(
+            `SELECT username
+            FROM users
+            WHERE username = $1`,
+            [username]
+        )
+        if(duplicateCheck.rows[0]){
+            throw new BadRequestError("That username already exists!")
+        } 
+
+        const result = await db.query(
+            `INSERT INTO users
+            (username,
+            password,
+            email,
+            first_name,
+            last_name,
+            birthday)
+            VALUES ($1, $2, $3, $4, $5, $6 )
+            RETURNING username, email, first_name AS "firstName", last_name AS "lastName, birthday`,
+            [
+                username, 
+                password, 
+                email, 
+                firstName, 
+                lastName, 
+                birthday
+            ]
+        )
+
+        const user = result.rows[0];
+        return user;
+    }
+
+    /** find a user by the username given
+     * returns {username, first_name, last_name, email, birthday}
+     * throws NotFoundError if user not found.
+     */
+
+    static async get(username){
+        const result = await db.query(
+            `SELECT username,
+                    password,
+                    email, 
+                    first_name AS "firstName", 
+                    last_name AS "lastName",
+                    birthday
+            FROM users
+            WHERE username = $1`,
+            [username]
+        );
+        
+        const user = result.rows[0];
+
+        if(!user){
+            throw new NotFoundError(`No user: ${username}`)
+        }
+    }
+}
--- reel-friends-seed.sql
+++ reel-friends-seed.sql
@@ -5,6 +5,7 @@ CREATE DATABASE reel_pals;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     hashed_password VARCHAR(255) NOT NULL,
     first_name VARCHAR(25),
     last_name VARCHAR(25),
