217fdae608b66f280ea381c1bbe240098fbce9cb
Christian Cruz <christian.cruz92@hotmail.com>
2024-08-01 19:54:54 -0400

    updated config file

--- db.js
+++ db.js
@@ -1,17 +1,10 @@
 // Database setup for Reel Pals
 
 const { Client } = require("pg");
-
-let DB_URI;
-
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///reel_pals_test";
-} else {
-  DB_URI = "postgresql:///reel_pals";
-}
+const { getDatabaseUri } = require("./config")
 
 let db = new Client({
-  connectionString: DB_URI
+  connectionString: getDatabaseUri()
 });
 
 db.connect();
