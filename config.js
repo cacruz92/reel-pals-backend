6976fe7e6339117403d5a65320250fcc3f30eb7e
Christian Cruz <christian.cruz92@hotmail.com>
2024-08-01 19:48:31 -0400

    Installed cors and morgan dependencies. added user routes to app.js and added configuration to app.js and config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+const { get } = require("http");
+
+require("dotenv").config();
+require("colors");
+const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+const PORT = +process.env.PORT || 3001;
+
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+console.log("Reel Pals Config:".green);
+console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+console.log("Database:".yellow, getDatabaseUri());
+console.log("---");
+
+function getDatabaseUri(){
+    return (process.env.NODE_ENV === "test")
+    ? "reel_pals_test"
+    : process.env.DATABASE_URL || "postgres://localhost:5432/reel_pals";
+}
+
+module.exports = {
+    SECRET_KEY,
+    PORT,
+    BCRYPT_WORK_FACTOR,
+    getDatabaseUri
+};
