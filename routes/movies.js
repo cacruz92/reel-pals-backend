67fedf11ff6f4bc7ba26a65ba2cc1be740562579
Christian Cruz <christian.cruz92@hotmail.com>
2024-08-03 06:55:27 -0400

    added route for adding movie

---
+++ routes/movies.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const Movie = ("../models/movie")
+
+const router = express.router();
+
+/** POST / { movie }  => { movie }
+ *
+ * Adds a new movie from the API to our database. 
+ *
+ * This returns the newly created movie:
+ *  {movie: { title, year, imdbId }}
+ *
+ **/
+
+router.post('/add', async(req, res, next) => {
+    try{
+        const {title, year, imdbId} = req.body;
+        const user = await Movie.addMovie({title, year, imdbId});
+        return res.status(201).json({movie})
+    } catch(e){
+        return next(e);
+    }
+})
